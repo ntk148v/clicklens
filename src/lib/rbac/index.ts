8c5d7e5aa66635b963fddb266c787b9ef5ec4301
Kien Nguyen Tuan <kiennt2609@gmail.com>
2025-12-30 14:29:33 +0700

    feat(rbac): add feature roles module with auto-create support

---
+++ src/lib/rbac/index.ts
@@ -0,0 +1,13 @@
+export {
+  FEATURE_ROLE_PREFIX,
+  FEATURE_ROLES,
+  DATA_PRIVILEGES,
+  getFeatureRole,
+  isFeatureRole,
+  filterOutFeatureRoles,
+  getFeatureRolesFromList,
+  isRestrictedDatabase,
+  type FeatureRole,
+  type DataPrivilege,
+  type DataPrivilegeType,
+} from "./capabilities";
