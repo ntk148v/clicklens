2d6d84c67c829bd8a802b96f18bd52290b7e486e
Kien Nguyen Tuan <kiennt2609@gmail.com>
2026-01-10 16:14:18 +0700

    add documentation

---
+++ docs/theme.config.jsx
@@ -0,0 +1,15 @@
+export default {
+  logo: <span>ClickLens</span>,
+  project: {
+    link: 'https://github.com/ntk148v/clicklens'
+  },
+  docsRepositoryBase: 'https://github.com/ntk148v/clicklens/tree/main/docs',
+  footer: {
+    text: 'ClickLens Documentation'
+  },
+  useNextSeoProps() {
+    return {
+      titleTemplate: '%s – ClickLens'
+    }
+  }
+}
