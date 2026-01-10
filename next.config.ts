215d533aa02803e444f2c2df61509774766d0000
Kien Nguyen Tuan <kiennt2609@gmail.com>
2026-01-10 14:04:43 +0700

    chore: add version

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
+import packageJson from "./package.json";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  env: {
+    NEXT_PUBLIC_APP_VERSION:
+      process.env.NEXT_PUBLIC_APP_VERSION || packageJson.version,
+  },
 };
 
 export default nextConfig;
